e1b8cf38118ca76d669ba66d412fed4209629c82
maxsteel001 <divyankweb@gmail.com>
2021-06-06 16:54:49 +0530

    minor ui fix check functionality for local storage

--- src/planet/Filter.js
+++ src/planet/Filter.js
@@ -2,23 +2,28 @@ import React from "react";
 import "./PlanetForm.css";
 
 function Filter({ heading, filterList, handleCheck, noTopPadding }) {
-  const persistCheck = (id, heading) => {
-    console.log(id);
+  const persistCheck = (event, id, name) => {
+    const { checked } = event.target;
+    //  console.log(checked);
+    localStorage.setItem(name, checked);
     if (heading === "Colors") {
     }
   };
-  const colorCheck = localStorage.getItem("selectedColorLocal").split(",");
+  const colorCheck = localStorage.getItem("selectedColorLocal")?.split(",");
+
   //console.log(colorCheck);
   return (
     <div className={`${!noTopPadding && "pt-30"} `}>
       <h1 className="heading pb-10">{heading}</h1>
       {filterList?.map((item, index) => (
         <div className="checkbox" key={item?.id}>
+          {console.log(localStorage.getItem(item.name), item.name)}
           <input
             type="checkbox"
-            value={persistCheck(item.id, heading)}
+            //checked={localStorage.getItem(item.name) === "true" ? true : false}
             onClick={(event) => {
               handleCheck(event, heading, item.id);
+              persistCheck(event, item.id, item.name);
             }}
           />
           <p>{item.name}</p>
