15d76084afe927d91f7299867a2ec080f8a525d9
maxsteel001 <divyankweb@gmail.com>
2021-06-06 18:36:12 +0530

    minor fix

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template.
 
-In this project I have setup the json server.
+In this project I have setup the json server. Initially the project loads the entire list of planets on first render.
+The list can be controlled by search filters.
 I have made the project using React Hooks.
 The code is fully responsive.
 I have tried to implement all the requirements.
--- src/planet/Filter.js
+++ src/planet/Filter.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./PlanetForm.css";
 
 function Filter({ heading, filterList, handleCheck, noTopPadding }) {
-  const persistCheck = (event, id, name) => {
+  /* const persistCheck = (event, id, name) => {
     const { checked } = event.target;
     //  console.log(checked);
     localStorage.setItem(name, checked);
     if (heading === "Colors") {
     }
-  };
-  const colorCheck = localStorage.getItem("selectedColorLocal")?.split(",");
+  };*/
+  //const colorCheck = localStorage.getItem("selectedColorLocal")?.split(",");
 
   //console.log(colorCheck);
   return (
@@ -17,13 +17,10 @@ function Filter({ heading, filterList, handleCheck, noTopPadding }) {
       <h1 className="heading pb-10">{heading}</h1>
       {filterList?.map((item, index) => (
         <div className="checkbox" key={item?.id}>
-          {console.log(localStorage.getItem(item.name), item.name)}
           <input
             type="checkbox"
-            //checked={localStorage.getItem(item.name) === "true" ? true : false}
             onClick={(event) => {
               handleCheck(event, heading, item.id);
-              persistCheck(event, item.id, item.name);
             }}
           />
           <p>{item.name}</p>
